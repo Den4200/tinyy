ae1936bd93005fa45127cedafe5542f422d898a2
Den4200 <dennis@dennispham.me>
2020-10-10 00:25:07 -0400

    Add PostgreSQL database connection and embed migrations.

---
+++ tinyy_backend/src/db.rs
@@ -0,0 +1,5 @@
+use rocket_contrib::databases::diesel;
+
+
+#[database("diesel_postgres_pool")]
+pub struct Conn(diesel::PgConnection);
--- tinyy_backend/src/lib.rs
+++ tinyy_backend/src/lib.rs
@@ -1,19 +1,51 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate diesel_migrations;
+
+#[macro_use]
 extern crate rocket;
 #[macro_use]
 extern crate rocket_contrib;
 
 use dotenv::dotenv;
 
+use rocket::Rocket;
+use rocket::fairing::AdHoc;
+
+mod db;
 mod config;
 mod routes;
 
-pub fn rocket() -> rocket::Rocket {
+
+embed_migrations!();
+
+
+pub fn run_migrations(rocket: Rocket) -> Result<Rocket, Rocket> {
+    let conn = db::Conn::get_one(&rocket).expect("Failed to get database connection..");
+
+
+    match embedded_migrations::run_with_output(&*conn, &mut std::io::stdout()) {
+        Ok(()) => {
+            println!("Successfully ran database migrations.");
+            Ok(rocket)
+        },
+        Err(e) => {
+            println!("Failed to run database migrations: {:?}", e);
+            Err(rocket)
+        }
+    }
+}
+
+
+pub fn rocket() -> Rocket {
     dotenv().ok();
 
     rocket::custom(config::from_env())
+        .attach(db::Conn::fairing())
+        .attach(AdHoc::on_attach("Database Migrations", run_migrations))
         .mount(
             "/",
             routes![
