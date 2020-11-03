ed49e9c71d052457f0cfecad8b9d26fe26e33023
Den4200 <dennis@dennispham.me>
2020-11-02 22:40:38 -0500

    Create user migration for database.

--- tinyy_backend/src/schema.rs
+++ tinyy_backend/src/schema.rs
@@ -4,3 +4,16 @@ table! {
         url -> Text,
     }
 }
+
+table! {
+    users (id) {
+        id -> Int4,
+        email -> Varchar,
+        password -> Varchar,
+    }
+}
+
+allow_tables_to_appear_in_same_query!(
+    tiny_urls,
+    users,
+);
