f6d317329b459423d6fbdab717ca58431c252db8
Den4200 <dennis@dennispham.me>
2020-11-09 18:23:33 -0500

    Create `NewUser` struct and fix some other small issues.

--- tinyy_backend/src/models/user.rs
+++ tinyy_backend/src/models/user.rs
@@ -1,15 +1,23 @@
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
+use diesel::result::{DatabaseErrorKind, Error};
 use serde::{Deserialize, Serialize};
-use validator::{Validate, ValidationError};
+use validator::Validate;
 use validator_derive::Validate;
 
 use crate::errors::UserError;
 use crate::schema::users;
 
-#[table_name = "users"]
-#[derive(Deserialize, Serialize, Queryable, Insertable, Validate)]
+#[derive(Debug, Serialize, Queryable)]
 pub struct User {
+    pub id: i32,
+    pub email: String,
+    pub password: String
+}
+
+#[derive(Debug, Deserialize, Insertable, Validate)]
+#[table_name = "users"]
+pub struct NewUser {
     pub id: Option<i32>,
     #[validate(email)]
     pub email: String,
@@ -18,14 +26,14 @@ pub struct User {
 }
 
 impl User {
-    pub fn new(user: User, conn: &PgConnection) -> Result<User, UserError> {
-        user
+    pub fn new(new_user: NewUser, conn: &PgConnection) -> Result<User, UserError> {
+        new_user
             .validate()
-            .map_err(|_| => UserError::InvalidUser)?;
+            .map_err(|_| UserError::InvalidUser)?;
         
         diesel::insert_into(users::table)
-            .values(&user)
-            .get_result(&conn)
+            .values(&new_user)
+            .get_result(conn)
             .map_err(|error| {
                 if let Error::DatabaseError(kind, _) = error {
                     if let DatabaseErrorKind::UniqueViolation = kind {
